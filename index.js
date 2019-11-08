d6cb19d20fe46f90ed55a369d0d0f0378b6faf4f
Carmen <ccurry20@gmail.com>
2019-11-07 21:24:43 -0500

    Completed MVP

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code away!
+const express = require("express");
+
+const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
+
+const server = express();
+server.use(express.json());
+
+function logger(req, res, next) {
+  console.log([new Date().toISOString()], ` ${req.method}, ${req.url}`);
+  next();
+}
+
+server.use(logger);
+server.use("/posts", postRouter);
+server.use("/users", userRouter);
+
+server.use("/", (req, res) => res.send("API up and running!"));
+
+server.listen(5000, () => console.log("API running on port 5000"));
